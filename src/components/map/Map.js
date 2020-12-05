ac86f9f735263ee6165236a4581bc4e8c7296ddd
Ralence <ranko.ostojic@gmail.com>
2020-12-05 11:43:06 +0100

    fix: disable scroll wheel zoom

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -102,6 +102,7 @@ export default class MyMap extends Component {
         <Map
           center={position}
           zoom={this.state.zoom}
+          scrollWheelZoom={false}
           sx={{
             height: "400px",
             width: "100%",
